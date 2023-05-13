d281aaca3bec428dc35c1f72dd8031d54ffa2780
Michael Ehab <michaelehab16@gmail.com>
2023-05-13 14:38:05 +0300

    Updated UrlNormalizer Implementation to be more robust

--- src/main/java/com/example/crawlify/utils/UrlNormalizer.java
+++ src/main/java/com/example/crawlify/utils/UrlNormalizer.java
@@ -1,41 +1,43 @@
 package com.example.crawlify.utils;
 
 import java.net.URI;
-import java.net.URL;
-import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UrlNormalizer {
 
-    private static final Pattern URL_PATTERN = Pattern.compile("^(https?://)?(www\\.)?[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)+([/?#].*)?$");
+    private static final Pattern URL_PATTERN = Pattern.compile(
+            "^(https?|ftp)://" // scheme
+                    + "([a-z0-9]+(-[a-z0-9]+)*\\.)+[a-z]{2,}" // hostname
+                    + "(:\\d+)?" // port
+                    + "(/[^?#]*)?" // path
+                    + "(\\?[^#]*)?" // query
+                    + "(#.*)?$"); // fragment
 
-    public static String normalize(String urlString) throws MalformedURLException, URISyntaxException {
-        // Validate the URL
+    public static String normalize(String urlString) throws URISyntaxException {
+        // Normalize the URL string
+        urlString = urlString.trim().replaceAll("\\s+", " ");
+
+        // Check if the URL is valid
         Matcher matcher = URL_PATTERN.matcher(urlString);
         if (!matcher.matches()) {
-            throw new MalformedURLException("Invalid URL: " + urlString);
+            throw new URISyntaxException(urlString, "Invalid URL format");
         }
 
-        // Convert the string to a URL object
-        URL url = new URL(urlString);
+        // Parse the URL into a URI object
+        URI uri = new URI(urlString);
 
-        // Get the scheme, host, port, path and query components
-        String scheme = url.getProtocol().toLowerCase();
-        String host = url.getHost().toLowerCase();
-        int port = url.getPort();
-        String path = url.getPath();
-        String query = url.getQuery();
-
-        // Create a URI object from the components
-        URI uri = new URI(scheme, null, host, port, path, query, null);
+        // If the URI is a relative URI, resolve it against a dummy base URI
+        if (!uri.isAbsolute()) {
+            uri = new URI("http://dummy.com").resolve(uri);
+        }
 
         // Normalize the URI
         uri = uri.normalize();
 
-        // Return the normalized URI as a string
+        // Convert the URI back to a string
+
         return uri.toString();
     }
-
 }
