de9eda57518871a009ecbd899871d4bce56f5423
ebramatef00 <ebraamatef@outlook.com>
2023-05-15 16:48:08 +0300

    added AutoComplete

--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -1,13 +1,36 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box, Center, Image, Input, Button } from "@chakra-ui/react";
 import logo from "../assets/logo/logo-black-no-background.png";
+import Autosuggest from "react-autosuggest";
 
 function Home() {
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState<string[]>([]);
 
-  const handleChange = (event: any) => {
+  const fetchSuggestions = async (value: string) => {
+    try {
+      const response = await fetch(`your-api-url?q=${value}`);
+      const data = await response.json();
+      setSuggestions(data.suggestions);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onInputChange = (event: React.FormEvent, { newValue }: Autosuggest.ChangeEvent) => {
+    setValue(newValue);
+  };
+
+  const renderSuggestion = (suggestion: string) => <div>{suggestion}</div>;
+
+  const onSuggestionSelected = (event: React.FormEvent, { suggestion }: Autosuggest.SuggestionSelectedEventData<string>) => {
+    navigate(`/results/${encodeURI(suggestion)}`);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
   };
 
@@ -15,30 +38,34 @@ function Home() {
     event.preventDefault();
     navigate(`/results/${encodeURI(query)}`);
   };
+  const getSuggestions = (value: string) => {
+    fetchSuggestions(value);
+  };
+
+  const inputProps = {
+    placeholder: "Enter your search query",
+    value,
+    onChange: onInputChange,
+  };
 
   return (
-    <Center h="100vh" flexDirection={"column"}>
-      <Image borderRadius="md" src={logo} alt="logo" height={"50vh"} />{" "}
-      <Box as="form" onSubmit={handleSubmit} mt={10} display="flex">
-        <Input
-          type="text"
-          value={query}
-          onChange={handleChange}
-          placeholder="Enter your search query"
-          borderRadius="5px"
-          width={"50vw"}
-        />
-        <Button
-          type="submit"
-          bg={"#0718C4"}
-          color={"white"}
-          ml={2}
-          borderRadius="5px"
-        >
-          Search
-        </Button>
-      </Box>
-    </Center>
+      <Center h="100vh" flexDirection={"column"}>
+        <Image borderRadius="md" src={logo} alt="logo" height={"50vh"} />
+        <Box as="form" onSubmit={handleSubmit} mt={10} display="flex">
+          <Autosuggest
+              suggestions={suggestions}
+              onSuggestionsFetchRequested={({ value }) => getSuggestions(value)}
+              onSuggestionsClearRequested={() => setSuggestions([])}
+              getSuggestionValue={(suggestion) => suggestion}
+              renderSuggestion={renderSuggestion}
+              inputProps={inputProps}
+              onSuggestionSelected={onSuggestionSelected}
+          />
+          <Button type="submit" bg={"#0718C4"} color={"white"} ml={2} borderRadius="5px">
+            Search
+          </Button>
+        </Box>
+      </Center>
   );
 }
 
